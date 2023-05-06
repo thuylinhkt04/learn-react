fe2fd847eb9b50a04b0458d2fa68d3ac944248af
linh77 <linh77@proton.me>
2023-05-06 15:15:41 +0700

    int

--- src/components/Lesson2/ListUsers/constants.js
+++
@@ -1,3 +0,0 @@
-export const START_LOADING = 'listUsers/start_loading';
-export const LOAD_USERS_SUCCESS = 'listUsers/load_users_success';
-export const DONE_LOADING = 'listUsers/done_loading';
--- src/components/Lesson2/UserDetail/constants.js
+++
@@ -1,3 +0,0 @@
-export const START_LOADING = 'userDetail/start_loading';
-export const LOAD_USER_SUCCESS = 'userDetail/load_user_success';
-export const DONE_LOADING = 'userDetail/done_loading';
